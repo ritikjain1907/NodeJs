a055be50b2e8e32b41c01d15a772dc4bf0eca6c5
ritikjain1907 <ritikjain1907@hotmail.com>
2022-06-04 16:13:37 +0530

    Added All_In_One in calculator

---
+++ Day 2/Calculator/allInOne.js
@@ -0,0 +1,15 @@
+module.exports = {
+    addition:function() {
+        console.log("Addition Function");
+    },
+    substraction:function() {
+        console.log("Substraction Function");
+    },
+    multiplication:function() {
+        console.log("Multiplication Function");
+    },
+    division:function() {
+        console.log("Division Function");
+    },
+    value : 100
+}
